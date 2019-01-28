bda11f447e7c5f69dc6a3d5407bd759a4a3d7b46
Keven-Zhu <zhukeven86@gmail.com>
2019-01-27 21:13:54 -0500

    First commit for assignment 1

---
+++ Falling Shapes/Assets/CameraChange.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject cOne;
+    public GameObject cTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchCamera();
+    }
+
+    void switchCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cameraChangeCounter();
+        }
+    }
+
+    void cameraChangeCounter()
+    {
+        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraPositionCounter++;
+        cameraPositionChange(cameraPositionCounter);
+    }
+
+    void cameraPositionChange(int camPosition)
+    {
+        if (camPosition > 1)
+        {
+            camPosition = 0;
+        }
+
+        //Set camera position database
+        PlayerPrefs.SetInt("CameraPosition", camPosition);
+
+        //Set camera position 1
+        if (camPosition == 0)
+        {
+            cOne.SetActive(true);
+            cTwo.SetActive(false);
+        }
+
+        //Set camera position 2
+        if (camPosition == 1)
+        {
+            cTwo.SetActive(true);
+            cOne.SetActive(false);
+        }
+
+    }
+}
---
+++ Falling Shapes/Assets/ResetPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    public Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.transform.position = startPos;
+        }
+    }
+}
