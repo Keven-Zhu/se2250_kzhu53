a3d12f38fdcf6515aa49e48d0209e8ee27e5d0f2
Keven-Zhu <zhukeven86@gmail.com>
2019-02-08 18:20:42 -0500

    Removed useless code

--- kzhu53_Assignment2/Assets/_Scripts/CubeGeneration.cs
+++ kzhu53_Assignment2/Assets/_Scripts/CubeGeneration.cs
@@ -17,11 +17,6 @@ public class CubeGeneration : MonoBehaviour
 
         for (int i = 0; i < numCubes; i++)
         {
-            /*
-            Vector3 pos = RandomCircle(centre, 5.0f);
-            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, centre - pos);
-            Instantiate(Cube, pos, rot);
-            */
             GameObject obj = Instantiate<GameObject>(Cube);
             obj.name = "Cube " + i;
 
@@ -41,31 +36,7 @@ public class CubeGeneration : MonoBehaviour
             int a = (360 / numCubes) * i;
             Vector3 pos = RandomCircle(centre, 4.0f, a);
             obj.transform.position = pos;
-            //Instantiate(Cube, pos, Quaternion.identity);
-
-            /*
-            if (i % 3 == 0)
-            {
-                Cube.GetComponent<Renderer>().material.color = colors[1];
-            }
-            else if (i % 2 == 0)
-            {
-                Cube.GetComponent<Renderer>().material.color = colors[2];
-            }
-            else
-            {
-                Cube.GetComponent<Renderer>().material.color = colors[0];
-            }
-            */
         }
-        /*
-        gameObjectList = new List<GameObject>();
-
-        for (numCubes = 1; numCubes < 9; numCubes++)
-        {
-            Instantiate(Cube);
-        }
-        */
     }
 
     Vector3 RandomCircle(Vector3 centre, float radius, int a)
@@ -78,19 +49,6 @@ public class CubeGeneration : MonoBehaviour
         
         return pos;
     }
-
-    /*
-    Vector3 RandomCircle (Vector3 centre, float radius)
-    {
-        float ang = Random.value * 360;
-        Vector3 pos;
-        pos.x = centre.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
-        pos.y = centre.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
-        pos.z = centre.z;
-
-        return pos;
-    }
-    */
 
     // Update is called once per frame
     void Update()
