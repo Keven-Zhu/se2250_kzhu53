9ef2839c7853ddb76464dd1bc8312851bf3b15ad
Keven-Zhu <zhukeven86@gmail.com>
2019-03-18 16:01:28 -0400

    Added Assignment 3

---
+++ kzhu53_Assignment3/Assets/__Scripts/Share.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Share : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private const string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+    private const string TWEET_LANGUAGE = "en";
+
+    public void shareToTwitter()
+    {
+        string text = "Highscore: ";
+        int score = ScoreManager.SCORE;
+        Application.OpenURL(TWITTER_ADDRESS +
+                    "?text=" + WWW.EscapeURL(text) + score +
+                    "&amp:lang=" + WWW.EscapeURL(TWEET_LANGUAGE));
+    }
+}
